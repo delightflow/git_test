5747c999c3e9ac2072da4d31b0d0f5e86d236439
delightflow <contact@patentwith.com>
2024-04-19 14:48:57 +0900

    feat: Rename remove_odd -> split_odd_even
    홀수를 제거하는 것이 아닌 홀수와 짝수를 각각의 리스트에 담아
    튜플로 반환하도록 변경하였습니다.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 def chech_available():
     return 'It works'
 
-def remov_odd(nums = [1,2,3,4]):
-    return [num for num in nums if num%2 == 0]
+def split_odd(nums = [1,2,3,4]):
+    odds = []
+    evens = []
+    for num in nums: 
+        if num % 2 == 0:
+            evens.append(num)
+        else: 
+            odds.append(num)
+        
+    return (odds, evens)
 
 if __name__ == '__main__':
-    print(remov_odd())
+    print(split_odd())
 
 
 
